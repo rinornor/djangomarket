73d754d2e29ef13f7b1f4a24d1565e6c8daf58c8
rinornor <rinornor@gmail.com>
2023-02-21 17:44:15 +0100

    Added the 'New Item' functionality

--- item/views.py
+++ item/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item
-
+from .forms import NewItemForm
 
 # Create your views here.
 
@@ -9,3 +10,23 @@ def detail(request, pk):
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
     context = {'item': item, 'related_items': related_items}
     return render(request, 'item/detail.html', context)
+
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit = False) # If we just save then the 'created_by' field is not added!
+            item.created_by = request.user
+            item.save()
+            return redirect('item:detail', pk = item.id)
+    else:
+        form = NewItemForm()
+
+    context ={
+        'form': form,
+        'title':'New item',
+    }
+    return render(request, 'item/form.html', context)
