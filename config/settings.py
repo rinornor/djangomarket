9c2f3022951d0b55a08f888966dff6bd084887cf
rinornor <rinornor@gmail.com>
2023-02-23 18:45:33 +0100

    Added Delete and Edit posibilities to a post.

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env 
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$wzjebshwz$fb9a7*#@*whit4=*2001=zu+h6$fpn+ew97l&@8"
+env = Env()
+env.read_env()
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default = False)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "decouple",
     "core", 
     "item",
     "dashboard",
--- item/views.py
+++ item/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 # Create your views here.
 
@@ -30,3 +30,32 @@ def new(request):
         'title':'New item',
     }
     return render(request, 'item/form.html', context)
+
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
+
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            item.save()
+            return redirect('item:detail', pk = item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    context ={
+        'form': form,
+        'title':'Edit item',
+    }
+    return render(request, 'item/form.html', context)
+
